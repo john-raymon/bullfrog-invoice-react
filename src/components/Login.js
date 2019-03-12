c73d502c85c3a3e0887cd5cb2f0b9c44899755fa
John Ray <john@johnraymon.com>
2019-03-12 13:46:42 -0400

    compose styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import { push } from 'connected-react-router'
 import { connect } from 'react-redux'
 
+import Arrow from '../images/arrow';
 import logo from '../images/small_logo.png';
-
 
 class Login extends Component {
   constructor(props) {
@@ -17,8 +17,8 @@ class Login extends Component {
 
   render() {
     return(
-      <div className="w-100 vh-100 flex flex-row items-center">
-        <div class="fl w-50 flex flex-column items-center">
+      <div className="w-100 vh-100 flex flex-row items-center measure-1024 center">
+        <div className="fl w-50 flex flex-column items-center">
           <p className="dinTitle">
             Welcome back,
             <br />
@@ -27,6 +27,15 @@ class Login extends Component {
               <img src={logo} width="25%" height="auto" />
             </span>
           </p>
+        </div>
+        <div className="fl w-50 flex flex-column items-center">
+          <div className="w-100 measure ph3">
+            <input className="InputField" type="email" value="" placeholder="Knack Email"/>
+            <input className="InputField" type="password" value="" placeholder="Knack Password" />
+            <p className="flex flex-row items-center dinLabel f7 blue self-start pointer">
+              SIGN IN <div className="ArrowIcon mh2"><Arrow /></div>
+            </p>
+          </div>
         </div>
       </div>
     )
---
+++ src/images/arrow.js
@@ -0,0 +1,15 @@
+import React from 'react';
+export default () => {
+  return (
+  <svg width="100%" height="100%" viewBox="0 0 11 21" >
+    <g id="Log-In-" stroke="none" stroke-width="1" fill-rule="evenodd" opacity="0.734551711" stroke-linecap="square">
+        <g id="Desktop-HD" transform="translate(-580.000000, -577.000000)" stroke="#979797">
+            <g id="arrow" transform="translate(585.500000, 587.500000) scale(-1, -1) translate(-585.500000, -587.500000) translate(580.000000, 578.000000)">
+                <path d="M0.189655172,9.5 L11,0" id="Line"></path>
+                <path d="M0.189655172,9.65322581 L10.8103448,18.8467742" id="Line"></path>
+            </g>
+        </g>
+    </g>
+  </svg>
+  )
+}
