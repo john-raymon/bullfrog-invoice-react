d412f33c5fd60ee48fc34b5d77295a26dcd9d1f8
John Ray <john@johnraymon.com>
2019-03-22 19:52:21 -0400

    remove extensions to conditonal if statement in localStorageMiddleware

--- src/util/configStore.js
+++ src/util/configStore.js
@@ -22,7 +22,6 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, createRootReducer(history))
 
 const localStorageMiddleware = store => next => action => {
-  const validActions = ["INIT_APP"]
   if (action.type === "LOGIN_FULLFILLED") {
     if (!action.error) {
       localforage.setItem('jwt', action.payload.token)
@@ -31,8 +30,6 @@ const localStorageMiddleware = store => next => action => {
   } else if (action.type === "LOGOUT" || action.type === "EXPIRED") {
     localforage.setItem('jwt', '');
     agent.setToken(null);
-  } else if ( validActions.includes(action.type) ) {
-    agent.setToken(store.getState().auth.token)
   }
   next(action);
 };
