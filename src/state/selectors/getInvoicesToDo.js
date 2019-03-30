7d142e472af098134ad43e296ab2b832c2938031
John Ray <john@johnraymon.com>
2019-03-29 20:19:24 -0400

    select first item of list invoice["field_570_raw"]

--- src/state/selectors/getInvoicesToDo.js
+++ src/state/selectors/getInvoicesToDo.js
@@ -9,7 +9,7 @@ export default createSelector(
   invoicesToDo => {
     console.log('invoices to do look like', invoicesToDo)
     const invoices = _get(invoicesToDo, "records", []).map((invoice, index) => {
-      const { id: customerId, identifier: customerName } = invoice["field_570_raw"]
+      const { id: customerId, identifier: customerName } = invoice["field_570_raw"][0]
       return {
         date: invoice["field_405"],
         customer: {
