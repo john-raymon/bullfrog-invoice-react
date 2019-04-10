e94250aacafea0829d65f2268c3453da96f7020c
John Ray <john@johnraymon.com>
2019-04-10 18:43:50 -0400

    add newRoomPROPS to state for CreateInvoice component

--- src/components/CreateInvoice.js
+++ src/components/CreateInvoice.js
@@ -9,7 +9,11 @@ class CreateInvoice extends Component {
       customerAddress: '',
       customerAddress: '',
       customerCityState: '',
-      dateOfLoss: "2019-01-01"
+      dateOfLoss: "2019-01-01",
+      newRoomName: '',
+      newRoomLength: '',
+      newRoomWidth: '',
+      newRoomHeight: ''
     }
   }
   render() {
@@ -151,36 +155,81 @@ class CreateInvoice extends Component {
             Rooms
           </p>
 
-          <input
-            className="InputField measure-1"
-            type="text"
-            name="newRoomName"
-            placeholder="Enter the room name"
-            value={this.state.newRoomName}
-          />
-          <label for="newRoomName">
-            <p className="dinLabel pa0 ma0 mb3 f6">
-              Room Name
-            </p>
-          </label>
+          <div className="Room__container">
+            <input
+              className="InputField measure-1"
+              type="text"
+              name="newRoomName"
+              placeholder="Enter the room name"
+              value={this.state.newRoomName}
+            />
+            <label for="newRoomName">
+              <p className="dinLabel pa0 ma0 mb3 f6">
+                Room Name
+              </p>
+            </label>
 
-          <div className="flex flex-row items-center justify-between">
-            <div className="flex flex-row items-center justify-between w-50">
-              <div className="w-30 flex flex-column items-center">
-                <input
-                  className="InputField tc"
-                  type="number"
-                  name="newRoomLength"
-                  placeholder="0"
-                  value={this.state.newRoomLength}
-                />
-                <label for="newRoomLength">
-                  <p className="dinLabel pa0 ma0 mb3 f6">
-                    Length
-                  </p>
-                </label>
+            <div className="flex flex-row items-center justify-between">
+
+              <div className="flex flex-row items-center justify-between w-50">
+
+                <div className="w-30 flex flex-column items-center">
+                  <input
+                    className="InputField tc"
+                    type="number"
+                    name="newRoomLength"
+                    placeholder="0"
+                    value={this.state.newRoomLength}
+                  />
+                  <label for="newRoomLength">
+                    <p className="dinLabel pa0 ma0 mb3 f6">
+                      Length
+                    </p>
+                  </label>
+                </div>
+
+                <div className="w-30 flex flex-column items-center">
+                  <input
+                    className="InputField tc"
+                    type="number"
+                    name="newRoomWidth"
+                    placeholder="0"
+                    value={this.state.newRoomWidth}
+                  />
+                  <label for="newRoomWidth">
+                    <p className="dinLabel pa0 ma0 mb3 f6">
+                      Width
+                    </p>
+                  </label>
+                </div>
+
+                <div className="w-30 flex flex-column items-center">
+                  <input
+                    className="InputField tc"
+                    type="number"
+                    name="newRoomHeight"
+                    placeholder="8"
+                    value={this.state.newRoomHeight}
+                  />
+                <label for="newRoomHeight">
+                    <p className="dinLabel pa0 ma0 mb3 f6">
+                      Height
+                    </p>
+                  </label>
+                </div>
+
               </div>
+
             </div>
+            <button
+              className="flex flex-row items-center dinLabel f7 mid-gray self-start pointer bt-0 bl-0 br-0 bg-transparent mt2"
+            >
+              ADD LINE ITEMS
+            </button>
+
+            <button className="GenericButton tc dinLabel pv3 f7 mt3">
+              ADD ROOM
+            </button>
           </div>
         </div>
       </div>
