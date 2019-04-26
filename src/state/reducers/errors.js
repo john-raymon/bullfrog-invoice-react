60da564b816b474c4d446007af0d9db647f0c369
John Ray <john@johnraymon.com>
2019-04-25 23:14:17 -0400

    add cases to switch statement for FETCH SETTINGS to errors reducers

--- src/state/reducers/errors.js
+++ src/state/reducers/errors.js
@@ -1,12 +1,47 @@
 const initialState = {
   dashboard: {
     invoicesToDo: null,
-    customers: null
-  }
+    customers: null,
+    allInvoices: null
+  },
+  settings: null
 }
 
 export default (state = initialState, { type, payload = {} } ) => {
   switch (type) {
+    // FETCH SETTINGS ERRORS
+    case "FETCH_SETTINGS_PENDING":
+    case "FETCH_SETTINGS_FULFILLED":
+    return {
+      ...state,
+      settings: null
+    }
+    case "FETCH_SETTINGS_REJECTED":
+    return {
+      ...state,
+      settings: payload
+    }
+
+    // FETCH ALL INVOICES ERRORS
+    case "FETCH_ALL_INVOICES_PENDING":
+    case "FETCH_ALL_INVOICES_FULFILLED":
+    return {
+      ...state,
+      dashboard: {
+        ...state.dashboard,
+        allInvoices: null
+      }
+    }
+    case "FETCH_ALL_INVOICES_REJECTED":
+    return {
+      ...state,
+      dashboard: {
+        ...state.dashboard,
+        allInvoices: payload
+      }
+    }
+
+    // FETCH INVOICES TO DO ERRORS
     case "FETCH_INVOICES_TO_DO_PENDING":
     case "FETCH_INVOICES_TO_DO_FULFILLED":
     return {
@@ -24,6 +59,8 @@ export default (state = initialState, { type, payload = {} } ) => {
         invoicesToDo: payload
       }
     }
+
+    // FETCH KNACK CUSTOMERS ERRORS
     case "FETCH_KNACK_CUSTOMERS_PENDING":
     case "FETCH_KNACK_CUSTOMERS_FULFILLED":
     return {
