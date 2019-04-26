062440a584f2bd870367507a3913cb41e904018e
John Ray <john@johnraymon.com>
2019-04-26 01:35:56 -0400

    update state/actions/dashboardActions: implement fetchAllInvoices action creator

--- src/state/actions/dashboardActions.js
+++ src/state/actions/dashboardActions.js
@@ -1,6 +1,28 @@
 import agent from '../../util/agent'
 import _debounce from 'lodash/debounce';
 
+export function fetchAllInvoices() {
+  return (dispatch, getState) => {
+    agent.setToken(getState().auth.token)
+    return dispatch({
+      type: "FETCH_ALL_INVOICES",
+      payload: agent.requests.get('invoices/all').then((res) => {
+          if (res.status === 401) {
+            dispatch({ type: "LOGOUT" })
+            return Promise.reject()
+          }
+          return res.invoices
+        })
+        .catch((err) => {
+          if (err.status === 401) {
+            dispatch({ type: "LOGOUT" })
+          }
+          return Promise.reject(err)
+        })
+    })
+  }
+}
+
 export function fetchInvoicesToDo() {
   return (dispatch, getState) => {
     const { token } = getState().auth
@@ -13,7 +35,6 @@ export function fetchInvoicesToDo() {
         // than 1 page, since we're expecting the Invoice-to-do Knack record to be
         // remove when saved as draft on our Backend
 
-        console.log('this is the res status!!!', res)
         if (res.status === 401) {
           dispatch({ type: "LOGOUT" })
           return Promise.reject()
