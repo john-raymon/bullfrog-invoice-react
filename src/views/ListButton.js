9602114beb27f62020bbfc6ed7e6a30e53ef0e2a
John Ray <john@johnraymon.com>
2019-04-30 18:45:58 -0400

    update views/ListButton : remove f8 custom util class, use tachyons f7

--- src/views/ListButton.js
+++ src/views/ListButton.js
@@ -5,7 +5,7 @@ import Arrow from '../images/arrow';
 
 export default function ListButton({children}) {
   return (
-    <button className="dim pointer dinLabel f8 mid-gray tracked-mega ttu ma0 mr1 pa0 bn bg-transparent flex flex-row items-center pv1 ttu">
+    <button className="dim pointer dinLabel f7 mid-gray tracked-mega ttu ma0 mr1 pa0 bn bg-transparent flex flex-row items-center pv1 ttu">
       {children}
       <div className="ArrowIcon mh2"><Arrow /></div>
     </button>
